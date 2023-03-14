c8651ef3758e9d2cfa19f582535614ed3f2a4658
Raúl Marrero <113340217+raulm1906@users.noreply.github.com>
2023-03-14 20:39:24 +0000

    Añadidos primeros tests de jsonToSQLImporter

---
+++ test/test_importSubjectsToSQL.py
@@ -0,0 +1,24 @@
+import unittest
+import json
+from jsonToSQLImporter.importSubjectsToSQL import deserializeSubject
+
+class TestJsonDeserialize(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        with open("../subjects.json", 'r', encoding='utf-8') as subjectsJson:
+            cls.subjectsList = json.load(subjectsJson)
+
+    def test_subject_name(self):
+        self.assertEqual(deserializeSubject(self.subjectsList[0])["name"], "ÁLGEBRA Y GEOMETRÍA")
+        self.assertEqual(deserializeSubject(self.subjectsList[-1])["name"], "PRÁCTICAS EXTERNAS")
+        self.assertEqual(deserializeSubject(self.subjectsList[3])["name"], "FUNDAMENTOS DE COMPUTADORES")
+
+
+
+# class TestSQLInsertString(unittest.TestCase):
+
+
+
+if __name__ == '__main__':
+    unittest.main()
