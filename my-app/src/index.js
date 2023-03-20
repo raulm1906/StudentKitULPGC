dafed5014d71af1c09aeaa050d034f7af0ed4b8b
Willy Escovilla Biason <willy.escovilla101@alu.ulpgc.es>
2023-03-20 23:00:31 +0000

    Deleted useless files and some non used packages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, BrowserRouter, Routes } from 'react-router-dom'
-import reportWebVitals from './reportWebVitals';
 
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '@chakra-ui/theme';
 import Layout from './pages/common/Layout'
-import AppRoutes from './AppRoutes';
-import Index from "./pages/common/Layout"
 import IndexHorario from "./pages/Horario/pages/HorarioEdit/IndexHorario"
 import ListHorario from './pages/Horario/ListHorario';
 
@@ -45,7 +42,3 @@ root.render(
   
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
