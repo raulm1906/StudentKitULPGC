a0aeb3bee84bae58d4c763966f7c11550701c8f3
unknown <eduetopa@gmail.com>
2023-03-21 16:08:03 +0000

    Unido nombre, apellido y añadida información

--- src/scrappers/profesorado_no_dis.py
+++ src/scrappers/profesorado_no_dis.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import lxml
+
 
 def obtener_informacion(url):
     page = requests.get(url)
@@ -8,34 +10,36 @@ def obtener_informacion(url):
     # Find the data table on the page
     table = soup.find('table')
     # Find all rows in the table
-    tr_elements = table.find_all('tr')
-
-    # Initialize a list to store the data rows
-    data_rows = []
-    count = 0
+    rows = table.find_all('tr')
     # Loop through all rows and get the name of the person in the first column
-    for tr in tr_elements:
-        try:
-            cols = tr.find_all('td')
-            # Skip rows with less than two columns or with certain text
-            if len(cols) < 2 or 'CORREO ELECTRÓNICO' in cols[0].text:
+    cont = 0
+    dicc = {}
+    nombre_completo = ''
+    for row in rows:
+        cols = row.find_all('td')
+
+        if len(cols) >= 2 and cont < 7:
+            if cont == 5:
+                cont += 1
                 continue
-            # Extract the data from the row
-            if len(cols) == 4:
-                data_rows.append((cols[0].text.strip(), cols[1].text.strip()))
-                data_rows.append((cols[2].text.strip(), cols[3].text.strip()))
+            if cont == 3:
+                datos = cols[2].text.strip()
+                resultado = cols[3].text.strip()
+                dicc[datos] = resultado
             else:
-                data_rows.append((cols[0].text.strip(), cols[1].text.strip()))
-            count += 1
-            if count >= 4:
-                break
-        except Exception as e:
-            print(f"Error processing row: {e}")
+                datos = cols[0].text.strip()
+                resultado = cols[1].text.strip()
 
-    # Use a dictionary comprehension to build the dictionary from the data rows
-    dicc = {k: v for k, v in data_rows}
+            if cont == 0:
+                nombre_completo += resultado + ' '
+            elif cont == 1:
+                nombre_completo += ', ' + resultado
+                dicc['profesor'] = nombre_completo
+
+            elif cont > 1:
+                dicc[datos] = resultado
+            cont += 1
     return dicc
-
 
 
 
@@ -52,7 +56,7 @@ for link in links:
     data.append(obtener_informacion(link.get('href')))
 
         # Store all the data in a JSON
-with open('json/teachers.json', 'w', encoding='utf-8') as f:
+with open('teachers.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4, ensure_ascii=False, sort_keys=True)
 
 
