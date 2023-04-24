0c6fb8f2b187a9da16459f86ac528f60c84cb88b
Guillermo <guillermoml2002@gmail.com>
2023-04-24 11:08:07 +0200

    Listo para merge

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {router} from './router'
-import  {Route, Router, RouterProvider} from "react-router-dom"
+import  {RouterProvider} from "react-router-dom"
 
 import { theme } from '@chakra-ui/theme';
 import { ChakraProvider } from '@chakra-ui/react';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
