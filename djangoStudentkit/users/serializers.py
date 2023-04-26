383205a86c54174d88b1daadc4bc8451f118a083
eduetopa <eduetopa@gmail.com>
2023-04-26 13:57:24 +0100

    mc

--- djangoStudentkit/users/serializers.py
+++ djangoStudentkit/users/serializers.py
@@ -6,7 +6,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined']
+        fields = ['email', 'first_name', 'last_name', 'is_active', 'is_staff', 'date_joined', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
     def create(self, validated_data):
--- djangoStudentkit/users/views.py
+++ djangoStudentkit/users/views.py
@@ -3,9 +3,9 @@ from .serializers import UserSerializer
 from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
-from rest_framework import generics
+from rest_framework import status, generics, viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 from django.contrib.auth import get_user_model
 
@@ -20,4 +20,8 @@ class UserList(generics.ListCreateAPIView):
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class LoginView(ObtainAuthToken):
+    
 
