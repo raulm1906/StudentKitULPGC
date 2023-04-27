53b4b705e8722cbaa66daf019d81fabdcaebeb4e
Raúl Marrero <113340217+raulm1906@users.noreply.github.com>
2023-04-27 20:50:40 +0100

    Finished Tutoring Hours Json Generator and added full profesorado.json

--- src/jsonUtils/tutoringHoursJsonGenerator.py
+++ src/jsonUtils/tutoringHoursJsonGenerator.py
@@ -5,22 +5,23 @@ import mariadb
 from src.insertSQL import DBConnection
 
 
-def createTutoringDict(tutoringHours: list) -> dict:
+def createTutoringDict(tutoringHours: list, semester: int) -> dict:
     tutoringData = {}
     tutoringData["teacher_id"] = teacher_id
     tutoringData["day"] = dayNumber + 1
     tutoringData["start_time"] = tutoringHours[0]
     tutoringData["ending_time"] = tutoringHours[1]
-    tutoringData["semester"] = 1
+    tutoringData["semester"] = semester+1
     return tutoringData
 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-connection = DBConnection.DBConnect(input("Host: "), input("Username: "), input("Password: "))
+connection = DBConnection.DBConnect(input("Username: "), input("Password: "), input("Host: "))
 with open("../scrappers/json/profesorado.json", 'r', encoding='utf-8') as teachersJson:
     teachersList = json.load(teachersJson)
 
-finalList = []
+finalData = []
+semestersNames = ("firstSemester", "secondSemester")
 
 cursor = connection.cursor()
 for teacher in teachersList:
@@ -33,20 +34,21 @@ for teacher in teachersList:
         print("Error in database select")
         sys.exit(1)
 
-    tutoring = teacher["tutorias"]
     days = ("LUNES", "MARTES", "MIÉRCOLES", "JUEVES", "VIERNES")
-    for dayNumber in range(5):
-        # Split preventivo en caso de que haya 2 horas de tutoría el mismo día
-        tutoringHoursList = tutoring[days[dayNumber]].split(" / ")
-        for tutoringHours in tutoringHoursList:
-            tutoringHours = tutoringHours.replace('.', ':')
-            # Comprueba que haya tutoría ese día
-            tutoringHours = tutoringHours.split("-")
-            if len(tutoringHours) == 2:
-                tutoringData = createTutoringDict(tutoringHours)
-                finalList.append(tutoringData)
+    tutoring = teacher["tutorias"]
+    for semester in range(2):
+        for dayNumber in range(4):
+            # Split preventivo en caso de que haya 2 horas de tutoría el mismo día
+            tutoringHoursList = tutoring[0][semestersNames[semester]][days[dayNumber]].split(" / ")
+            for tutoringHours in tutoringHoursList:
+                tutoringHours = tutoringHours.replace('.', ':')
+                # Comprueba que haya tutoría ese día
+                tutoringHours = tutoringHours.split("-")
+                if len(tutoringHours) == 2:
+                    tutoringData = createTutoringDict(tutoringHours,semester)
+                    finalData.append(tutoringData)
+with open('../scrappers/json/tutoring_hours.json', 'w', encoding='utf-8') as f:
+    json.dump(finalData, f, indent=4, ensure_ascii=False)
 
-with open('../scrappers/json/tutoring_hours_1sem.json', 'w', encoding='utf-8') as f:
-    json.dump(finalList, f, indent=4, ensure_ascii=False)
 
 DBConnection.DBDisconnect(connection)
