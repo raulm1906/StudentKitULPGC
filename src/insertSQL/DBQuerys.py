0d820fac6c62877639f93ddc3bb6575e0aeddc6e
Raúl Marrero <raulm1906@gmail.com>
2023-04-28 14:59:40 +0100

    hotfix DBQuerys.py

--- src/insertSQL/DBQuerys.py
+++ src/insertSQL/DBQuerys.py
@@ -9,10 +9,7 @@ def getTeacher_id(tacherName, connection: DBConnection) -> int:
         cursor.execute(f"SELECT id FROM teacher WHERE name='{tacherName}'")
         teacher_id = cursor.fetchone()[0]
     except mariadb.Error as e:
-        DBConnection.DBDisconnect(connection)
         print("Error in database select")
-        sys.exit(1)
-    DBConnection.DBDisconnect(connection)
     return teacher_id
 
 def getSubject_id(subjectName, connection: DBConnection) -> int:
@@ -23,8 +20,5 @@ def getSubject_id(subjectName, connection: DBConnection) -> int:
         cursor.execute(f"SELECT code FROM subject WHERE name='{subjectName}'")
         subject_id = cursor.fetchone()[0]
     except mariadb.Error as e:
-        DBConnection.DBDisconnect(connection)
         print("Error in database select")
-        sys.exit(1)
-    DBConnection.DBDisconnect(connection)
     return subject_id
