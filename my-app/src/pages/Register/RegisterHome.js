47ce97f6c07ccd08d9860564d28e6ba68dee0311
loukavanhoucke480@gmail.com <loukavanhoucke480@gmail.com>
2023-04-29 17:25:36 +0000

    login-register/style

--- my-app/src/pages/Register/RegisterHome.js
+++ my-app/src/pages/Register/RegisterHome.js
@@ -1,76 +1,88 @@
-import React from "react";
-import { useState } from "react";
-import { FormControl, FormLabel, FormHelperText, FormErrorMessage, Input, Checkbox } from "@chakra-ui/react";
-import { Button, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Button, Box, Text } from "@chakra-ui/react";
 import '../../components/forms.css'
 import imagen from "./Group 22.svg";
 import RegisterPortada from "./RegisterPortada";
 import { Link } from "react-router-dom";
 
 function RegisterHome() {
-    const [usuarioInput, setUsuarioInput] = useState('');
-    const [emailInput, setEmailInput] = useState('');
-    const [passwordInput, setPasswordInput] = useState('');
-    const [confirmPasswordInput, setConfirmPasswordInput] = useState('');
+  const [usuarioInput, setUsuarioInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState('');
+  const [isPasswordMatch, setIsPasswordMatch] = useState(true);
+  const [isFormValid, setIsFormValid] = useState(false);
 
-    const handleUsuarioInputChange = (e) => setUsuarioInput(e.target.value);
-    const handleEmailInputChange = (e) => setEmailInput(e.target.value);
-    const handlePasswordInput = (e) => setPasswordInput(e.target.value);
-    const handleConfirmPasswordInput = (e) => setConfirmPasswordInput(e.target.value);
+  const handleUsuarioInputChange = (e) => setUsuarioInput(e.target.value);
+  const handleEmailInputChange = (e) => setEmailInput(e.target.value);
+  const handlePasswordInputChange = (e) => {
+    setPasswordInput(e.target.value);
+    setIsPasswordMatch(e.target.value === confirmPasswordInput);
+  };
+  const handleConfirmPasswordInputChange = (e) => {
+    setConfirmPasswordInput(e.target.value);
+    setIsPasswordMatch(e.target.value === passwordInput);
+  };
 
-    const isUsuarioError = usuarioInput === ''
-    const isEmailError = emailInput === ''
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isFormValid) {
+      console.log("Form submitted");
+    } else {
+      console.log("Form not valid");
+    }
+  }
 
-    return (
-      <Box colSpan={2} display={"flex"}>
-        <div className=" text-center w-100 register">
-          <h1>Crea una cuenta</h1>
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+  const validateForm = () => {
+    if (!usuarioInput || !emailInput || !passwordInput || !confirmPasswordInput) {
+      return false;
+    }
+
+    if (!validateEmail(emailInput)) {
+      return false;
+    }
+
+    if (!isPasswordMatch) {
+      return false;
+    }
+
+    return true;
+  }
+
+  const handleFormChange = () => {
+    setIsFormValid(validateForm());
+  }
+
+  return (
+    <Box colSpan={2} display={"flex"}>
+      <div className=" text-center w-100 register">
+        <h1>Crea una cuenta</h1>
+        <form onSubmit={handleSubmit}>
           <FormControl>
             <FormLabel className="usuario-label">Nombre de usuario</FormLabel>
-            <Input type='usuario' value={usuarioInput} onChange={handleUsuarioInputChange}/>
-            {!isUsuarioError ? (
-              <FormHelperText>
-                Enter your username.
-              </FormHelperText>
-            ) : (
-              <FormErrorMessage>Username is required.</FormErrorMessage>
-            )}
+            <Input placeholder="Pepe Fernandez" type='text' value={usuarioInput} onChange={handleUsuarioInputChange} onBlur={handleFormChange} required/>
             <FormLabel className="email-label">Email</FormLabel>
-              <Input placeholder="pepe.fernandez110@alu.ulpgc.es" type="email" value={emailInput} onChange={handleEmailInputChange} />
-              {!isEmailError ? (
-                <FormHelperText>
-                  Enter the email you'd like to receive the newsletter on.
-                </FormHelperText>
-              ) : (
-                <FormErrorMessage>Email is required.</FormErrorMessage>
-              )}
+            <Input placeholder="pepe.fernandez110@alu.ulpgc.es" type="email" value={emailInput} onChange={handleEmailInputChange} onBlur={handleFormChange} required/>
             <FormLabel className="contraseña-label">Contraseña</FormLabel>
-            <Input type='password' value={passwordInput} onChange={handlePasswordInput} />
-            {!isEmailError ? (
-              <FormHelperText>
-                Enter the email you'd like to receive the newsletter on.
-              </FormHelperText>
-            ) : (
-              <FormErrorMessage>Email is required.</FormErrorMessage>
-            )}
+            <Input type='password' value={passwordInput} onChange={handlePasswordInputChange} onBlur={handleFormChange} required/>
             <FormLabel className="confirmarContraseña-label">Confirmar contraseña</FormLabel>
-            <Input type='password' value={confirmPasswordInput} onChange={handleConfirmPasswordInput} />
-            {!isEmailError ? (
-              <FormHelperText>
-                Enter the email you'd like to receive the newsletter on.
-              </FormHelperText>
-            ) : (
-              <FormErrorMessage>Email is required.</FormErrorMessage>
-            )}
+            <Input type='password' value={confirmPasswordInput} onChange={handleConfirmPasswordInputChange} onBlur={handleFormChange} required/>
+            {!isPasswordMatch && <Text color="red">Las contraseñas no coinciden</Text>}
           </FormControl>
-          <Button>Registrarse</Button>
-          <p>¿Ya tienes una cuenta?</p>
-          <Link to="/Login">Login</Link>
+          <Button type="submit" disabled={!isFormValid}>Registrarse</Button>
+        </form>
+        <p>¿Ya tienes una cuenta?</p>
+        <Link to="/Login">Login</Link>      
         </div>
-        <RegisterPortada></RegisterPortada>
-      </Box>
-    )
-
+      <RegisterPortada />
+    </Box>
+  )
 }
 
-export default RegisterHome
+export default RegisterHome;
