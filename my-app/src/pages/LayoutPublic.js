28258971fc0de5bc8babe74b256c118faaa06ae8
WillyEB <thewillyeb@gmail.com>
2023-05-01 13:40:43 +0100

    Fixed some responsive errors

--- my-app/src/pages/LayoutPublic.js
+++ my-app/src/pages/LayoutPublic.js
@@ -1,13 +1,25 @@
 import { Outlet } from "react-router-dom";
 import Header from "./cabecera";
 import Footer from "../components/Footer";
+import { Grid, GridItem } from '@chakra-ui/react'
 
 const LayoutPublic = () =>{
     return(
     <>
-        <Header />
-        <Outlet/>
-        <Footer />
+        <Grid
+                h="100vh"
+                templateRows="auto auto 1fr auto"
+                templateColumns="repeat(3, 1fr)"
+                gap={4}
+            >
+                <GridItem gridRow={1} colSpan={3}>
+                    <Header />
+                </GridItem>
+                <Outlet/>
+                <GridItem gridRow={4} colSpan={3}>
+                    <Footer />
+                </GridItem>
+            </Grid>
 
     </>
     )
