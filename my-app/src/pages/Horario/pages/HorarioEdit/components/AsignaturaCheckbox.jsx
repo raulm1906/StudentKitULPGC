2331e206b8ee96b26eb33aeb13677f85092d2b0c
WillyEB <thewillyeb@gmail.com>
2023-05-02 20:21:22 +0100

    a

--- my-app/src/pages/Horario/pages/HorarioEdit/components/AsignaturaCheckbox.jsx
+++ my-app/src/pages/Horario/pages/HorarioEdit/components/AsignaturaCheckbox.jsx
@@ -9,19 +9,33 @@ import { createEvents } from '../utils/utils'
 
 
 export default function AsignaturaCheckbox({ subject }) {
-    console.log(subject)
-    const datos = "assds"
-    {/* Handles state of the checkbox */}
+    
+    const lessons = subject.lessons
+    console.log(lessons)
+
+    const groupMapping = {
+        41: 17,
+        42: 17,
+        43: 18,
+        44: 18,
+        45: 19,
+        46: 19,
+        47: 20,
+        48: 20,
+        49: 21,
+        50: 21,
+        51: 22,
+        52: 22
+      }
+
     const [isChecked, setIsChecked] = useState(false)
-
-    {/* Handles the list of events that will be displayed on the schedule */}
     const {events, setEvents} = useContext(AppContext)
-
-    { /* Handles the state of selected theory group */}
     const [groupTheory, setGroupTheory] = useState(null)
-
-    { /* Handles the state of selected practice group */}
     const [groupPractice, setGroupPractice] = useState(null)
+
+    function getActualGroup(group) {
+        return groupMapping[group] || group;
+    }
 
     function handleTheory(group){
         setGroupTheory(group)
@@ -32,17 +46,13 @@ export default function AsignaturaCheckbox({ subject }) {
     }    
 
     function handleNewEvents(){
-        const sessionsTheory = datos.filter(item => item.Group === groupTheory)
-        const sessionsPractice = datos.filter(item => item.Group === groupPractice)
-
-        const eventsTheory = createEvents(sessionsTheory, 40951)
-        const eventsPractice = createEvents(sessionsPractice, 40951)
-
-        setEvents([...events, ...eventsTheory, ...eventsPractice])
+        console.log("New event")
+        const sessionsTheory = lessons.filter(item => item.group === groupTheory)
+        const sessionsPractice = lessons.filter(item => item.Group === getActualGroup(groupPractice))
     }
 
     function handleDeleteEvents(id){
-        setEvents(events.filter(event => event.subject_code !== 40951));
+        console.log("Delete event")
     }
 
     function handleCheckBoxChange(e){
@@ -82,7 +92,9 @@ export default function AsignaturaCheckbox({ subject }) {
                             {groupTheory}
                         </MenuButton>
                         <MenuList>
-                           </MenuList>
+                            <MenuItem onClick={() => handleTheory(1)}>Grupo 1</MenuItem>
+                            <MenuItem onClick={() => handleTheory(2)}>Grupo 2</MenuItem>
+                        </MenuList>
                     </Menu>
                 </Flex>
                 <Flex flexDirection="column" marginLeft="10px" marginRight="10px" alignItems="center">
@@ -105,7 +117,14 @@ export default function AsignaturaCheckbox({ subject }) {
                             {groupPractice}
                         </MenuButton>
                         <MenuList>
-
+                            <MenuItem onClick={() => handlePractice(41)}>Grupo 41</MenuItem>
+                            <MenuItem onClick={() => handlePractice(42)}>Grupo 42</MenuItem>
+                            <MenuItem onClick={() => handlePractice(43)}>Grupo 43</MenuItem>
+                            <MenuItem onClick={() => handlePractice(44)}>Grupo 44</MenuItem>
+                            <MenuItem onClick={() => handlePractice(45)}>Grupo 45</MenuItem>
+                            <MenuItem onClick={() => handlePractice(46)}>Grupo 46</MenuItem>
+                            <MenuItem onClick={() => handlePractice(47)}>Grupo 47</MenuItem>
+                            <MenuItem onClick={() => handlePractice(48)}>Grupo 48</MenuItem>
                         </MenuList>
                     </Menu>
                 </Flex>
