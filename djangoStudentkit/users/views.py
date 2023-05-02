b67b429ae9c4ebbfe336661e63aa024c6b287a42
eduetopa <eduetopa@gmail.com>
2023-05-02 17:33:44 +0100

    mc

--- djangoStudentkit/users/views.py
+++ djangoStudentkit/users/views.py
@@ -1,7 +1,7 @@
 from .models import User
 from .serializers import UserSerializer#, LoginSerializer
 from django.http import JsonResponse, HttpResponse
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics, viewsets, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -9,11 +9,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 
 User = get_user_model()
-
+'''
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -44,3 +44,50 @@ class InicioSesionAPI(generics.GenericAPIView):
             'access': str(refresh.access_token),
         }
         return Response(data)
+'''
+
+class UsuarioRegistroView(APIView):
+    """
+    Vista para registrar un nuevo usuario
+    """
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UsuarioInicioSesionView(APIView):
+    """
+    Vista para iniciar sesión en la aplicación y obtener un token de autenticación
+    """
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data['email']
+            password = serializer.validated_data['password']
+            user = authenticate(username=email, password=password)
+            if user:
+                login(request, user)
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'token': token.key})
+        return Response({'error': 'Nombre de usuario o contraseña incorrectos'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UsuarioCierreSesionView(APIView):
+    """
+    Vista para cerrar la sesión y revocar el token de autenticación
+    """
+
+    def post(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, Token.DoesNotExist):
+            pass
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
