0d747b5f17b86f45bb40ad3775cb9ada2785bea6
eduetopa <eduetopa@gmail.com>
2023-05-08 10:44:50 +0100

    mc

--- djangoStudentkit/config/settings/local.py
+++ djangoStudentkit/config/settings/local.py
@@ -3,9 +3,12 @@ from .base import *
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
+        #"NAME": BASE_DIR / 'db.sqlite3',
+
         "OPTIONS": {
             "read_default_file": os.path.join(BASE_DIR, 'config/settings/my.cnf'),
         },
+
     }
 }
 REST_FRAMEWORK = {
