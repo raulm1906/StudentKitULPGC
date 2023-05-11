ffa44d0a5c8cb4393195286377ced1e2ce4d565d
eduetopa <eduetopa@gmail.com>
2023-05-11 16:13:18 +0100

    fix

--- djangoStudentkit/config/settings/local.py
+++ djangoStudentkit/config/settings/local.py
@@ -2,8 +2,8 @@ from .base import *
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / 'config/db.sqlite3',
     
         #"OPTIONS": {
         #    "read_default_file": os.path.join(BASE_DIR, 'config/settings/my.cnf'),
